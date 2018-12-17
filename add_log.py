8e600f68fba600fa1678750d45176fb37ac57bec
jordancar <jordancar@126.com>
2018-12-17 10:35:49 +0800

    change fileencoding utf8

--- add_log.py
+++ add_log.py
@@ -5,5 +5,5 @@ logger.setLevel(logging.DEBUG)
 LOG_FORMAT=logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 st=logging.StreamHandler()
 st.setFormatter(LOG_FORMAT)
-# st.setLevel(logging.CRITICAL) #关闭所有info和debug日志输出
+# st.setLevel(logging.CRITICAL) #鍏抽棴鎵€鏈塱nfo鍜宒ebug鏃ュ織杈撳嚭
 logger.addHandler(st)
